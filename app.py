45d21704b9cd6930d824cc1754317dcfd66c98d7
snake1515 <regarcia1515@gmail.com>
2026-05-16 22:34:55 -0500

    fix flask host

--- app.py
+++ app.py
@@ -166,12 +166,5 @@ def descargar():
     )
 
 
-if __name__ == '__main__':
-
-    init_db()
-
-    app.run(
-        debug=True,
-        host='0.0.0.0',
-        port=5000
-    )
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
